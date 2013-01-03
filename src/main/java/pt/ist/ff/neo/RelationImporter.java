e5bd0cf602ef9745aa3fa7a609a4862b4b0f5873
Joao Carvalho <joao.pedro.carvalho@ist.utl.pt>
2013-01-03 12:11:30 +0000

    Relations step one. For now only works on one-to-many

--- src/main/java/pt/ist/ff/neo/RelationImporter.java
+++ src/main/java/pt/ist/ff/neo/RelationImporter.java
@@ -1,16 +1,106 @@
 package pt.ist.ff.neo;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.RelationshipType;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.index.Index;
 
+import dml.DomainClass;
 import dml.DomainRelation;
+import dml.Role;
 
 public class RelationImporter {
 
-    public static void importRelation(GraphDatabaseService db, DomainRelation relation, Connection con) throws SQLException {
+    private static final Logger logger = LogManager.getLogger(RelationImporter.class);
 
+    public static void importRelation(GraphDatabaseService db, final DomainRelation relation, Connection con) throws SQLException {
+
+	Index<Node> oidIndex = db.index().forNodes("oid");
+
+	Role dataRole = getDataRole(relation);
+
+	if (dataRole == null) {
+	    logger.warn("Ignoring many-to-many relation for now: " + relation.getFullName());
+	    return;
+	}
+
+	Role otherRole = getOtherRole(relation);
+
+	RelationshipType type = new RelationshipType() {
+
+	    @Override
+	    public String name() {
+		return relation.getName();
+	    }
+	};
+
+	DomainClass classToGet = Util.getTopClass((DomainClass) dataRole.getType());
+
+	String dbName = Util.getDBName(classToGet.getName());
+
+	String relationCol = "OID_" + Util.getDBName(otherRole.getName());
+
+	logger.info("Importing relation " + relation.getName());
+
+	logger.trace("\tRelation is on table " + dbName + ". Column: " + relationCol);
+
+	try (Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery("SELECT OID, " + relationCol + " FROM " + dbName + " WHERE " + relationCol
+			+ " IS NOT NULL")) {
+
+	    int count = 0;
+
+	    Transaction tx = db.beginTx();
+
+	    while (rs.next()) {
+		count++;
+
+		Long oid = rs.getLong("OID");
+		Long otherOid = rs.getLong(relationCol);
+
+		Node one = oidIndex.get("oid", oid).getSingle();
+		Node other = oidIndex.get("oid", otherOid).getSingle();
+
+		if (one == null || other == null) {
+		    logger.error("\tNode does not exist! Original OID: " + oid + ". Other OID : " + otherOid);
+		    logger.error("One: " + one + " Other: " + other);
+		    continue;
+		}
+
+		one.createRelationshipTo(other, type);
+		other.createRelationshipTo(one, type);
+	    }
+
+	    tx.success();
+	    tx.finish();
+
+	    logger.info("Finished importing relations. Imported " + count + " instances.");
+
+	}
+
+    }
+
+    private static Role getOtherRole(DomainRelation relation) {
+	return getDataRole(relation);
+    }
+
+    private static Role getDataRole(DomainRelation relation) {
+
+	if (relation.getFirstRole().getMultiplicityUpper() != Role.MULTIPLICITY_MANY)
+	    return relation.getFirstRole();
+
+	if (relation.getSecondRole().getMultiplicityUpper() != Role.MULTIPLICITY_MANY)
+	    return relation.getSecondRole();
+
+	return null;
     }
 
 }
